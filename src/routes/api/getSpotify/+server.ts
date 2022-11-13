fc11d7baceefc7425386158370e8f5d34f65204a
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-11-13 14:34:52 +0000

    api changes

--- src/routes/api/getSpotify/+server.ts
+++ src/routes/api/getSpotify/+server.ts
@@ -1,6 +1,19 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
- 
-export const GET: RequestHandler = ({ url, request }) => {
-  return new Response('hello')
-}
+import { Spotify } from '../../../classes/spotify';
+
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+const refreshToken = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
+
+const spotify: any = new Spotify(refreshToken, clientId, clientSecret);
+
+export const GET: RequestHandler = async ({ url, request }) => {
+	const query = url.searchParams.get('query');
+	const req = await request.json()
+	const parse = await spotify.search({query, type: 'track'})
+
+	console.log(parse.tracks.items);
+
+	return new Response('hello');
+};
